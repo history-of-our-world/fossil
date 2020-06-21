15ee0aad5000d28dbb25782bbb8d4f75bd807104
Dmytry <dmytro.tkachuk@nure.ua>
2020-06-21 16:19:04 +0300

    init app

---
+++ build/precache-manifest.914e75b2fa5f8065c8aeef70cab5b049.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f1c45e28f6204cc3fbcbff69412f0ff",
+    "url": "/likes/index.html"
+  },
+  {
+    "revision": "09f68bec98e399028d8d",
+    "url": "/likes/static/css/main.911e9b91.chunk.css"
+  },
+  {
+    "revision": "a2d3f541742fc43f0353",
+    "url": "/likes/static/js/2.48bc9833.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/likes/static/js/2.48bc9833.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09f68bec98e399028d8d",
+    "url": "/likes/static/js/main.e3204c63.chunk.js"
+  },
+  {
+    "revision": "ce761130d22b35b1b37b",
+    "url": "/likes/static/js/runtime-main.a3365554.js"
+  }
+]);
